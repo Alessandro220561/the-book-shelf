d58defbfa48b240083508d47fcdfe3a88e2f486b
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-06-22 12:40:03 -0400

    data rendered onto webpage

--- src/BookCard.js
+++ src/BookCard.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const BookCard = ({ book }) => {
 
-    
-
     return (
-        <div>
-            
-        </div>
+        <li className="card">
+            <img src={book.image} alt={book.title}/>
+            <h3>{book.title}</h3>
+            <h4>{book.author}</h4>
+            <p>{book.publisher}</p>
+            <p>Price : $22.99</p>
+        </li>
     )
 
 };
