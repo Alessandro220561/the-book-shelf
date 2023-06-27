216c864d5341fc4d14503eff17313a29f47004c5
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-06-27 10:32:46 -0400

    created Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to Book-Finds</h1>
+            <h1>Welcome to <strong>Book-Finds</strong></h1>
             <p>Browse through our collection of books</p>
             <p>Choose your favorite books and list and sell your own books if you choose to!</p>
             <p>HAPPY READING!</p>
